4478eb2117495e4ef15bf65534abf5a4d87396ef
AnnaertLars <lars.annaert@student.hogent.be>
2025-11-12 15:57:36 +0100

    Update Sensor_2.0.ino

--- code/proof of concepts/sensor/Sensor_2.0.ino
+++ code/proof of concepts/sensor/Sensor_2.0.ino
@@ -1,50 +1,32 @@
-#include "BluetoothSerial.h"  // Voor Bluetooth-functionaliteit
+#include <QTRSensors.h>
 
-BluetoothSerial BTSerial;     // Bluetooth-instantie
+QTRSensors qtr;
 
-// GPIO's voor de QTR-8A (ik gebruik er 6)
-const int sensorPins[6] = {36, 39, 34, 35, 32, 33};
-int sensorValues[6];          // Opslag voor sensorwaarden
+const uint8_t SensorCount = 6;
+uint16_t sensorValues[SensorCount];
 
-bool debugMode = false;       // Variabele om te controleren of debug aanstaat
+void setup()
+{
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, SensorCount);
+  qtr.setEmitterPin(2);
 
-void setup() {
-  // Start seriële en Bluetooth-communicatie
-  Serial.begin(115200);
-  BTSerial.begin("ESP32_LineFollower");  // Naam van je ESP32 voor Bluetooth
-  Serial.println("Bluetooth gestart! Koppel met 'ESP32_LineFollower'.");
+  Serial.begin(9600);
 }
 
-void loop() {
-  // Controleer op binnenkomende Bluetooth-commando's
-  if (BTSerial.available()) {
-    String command = BTSerial.readStringUntil('\n'); // Lees het Bluetooth-commando
-    command.trim(); // Verwijder spaties en nieuwe regels
 
-    if (command == "debug") {
-      debugMode = !debugMode; // Wissel debugmodus aan/uit
-      BTSerial.println(debugMode ? "Debugmodus AAN" : "Debugmodus UIT");
-    } else {
-      BTSerial.println("Onbekend commando. Gebruik 'debug' om sensoren te lezen.");
-    }
+void loop()
+{
+  // Lezen sensorwaardes
+  qtr.read(sensorValues);
+
+  // Schrijven van sensorwaardes (0 tot 1023), met 0 = max reflectie en 1023 = min reflectie
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
   }
+  Serial.println();
 
-  // Lees sensorgegevens als debugmodus aanstaat
-  if (debugMode) {
-    for (int i = 0; i < 6; i++) {
-      sensorValues[i] = analogRead(sensorPins[i]);
-    }
-
-    // Stuur sensorgegevens via Bluetooth
-    BTSerial.print("Sensorwaarden: ");
-    for (int i = 0; i < 6; i++) {
-      BTSerial.print("S");
-      BTSerial.print(i);
-      BTSerial.print("=");
-      BTSerial.print(sensorValues[i]);
-      if (i < 5) BTSerial.print(" | "); // Komma/pipe tussen waarden
-    }
-    BTSerial.println(); // Nieuwe regel
-    delay(500);         // Wacht 500 ms tussen uitlezingen    
-  }
+  delay(250);
 }
